3332ba7888a6d14fb8396c3f1c515ca50c765081
Mikhail Safronov <misha.safronov.02.12@mail.ru>
2023-06-07 05:22:32 +0300

    /

--- AdBoardsDesktop/Models/db/Context.cs
+++ AdBoardsDesktop/Models/db/Context.cs
@@ -7,7 +7,7 @@ namespace AdBoardsDesktop.Models.db;
 
 public partial class Context 
 {
-    public static AdBoardsApiClient Api = new AdBoardsApiClient("https://adboards.site/api");
+    public static AdBoardsApiClient Api = new AdBoardsApiClient("https://adboards.site/api/");
     public static AuthorizedModel? UserNow { get; set; }
     public static Ad? AdNow { get; set; }
     public static AdListViewModel? AdList { get; set; } = null;
